b7cf438619806435c4cc615f0c36c996360fe200
Guilherme Bacin Hermes <hermes@MacBook-Air-de-Guilherme.local>
2026-09-16 21:24:08 -0300

    Adicionando a main ao projeto.

---
+++ main.py
@@ -0,0 +1,15 @@
+from matematica import par_ou_impar
+
+def main():
+
+    numero = int(input("Digite um número inteiro: "))
+    resultado = par_ou_impar(numero)
+    print(f"O número {numero} é {resultado}.")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
